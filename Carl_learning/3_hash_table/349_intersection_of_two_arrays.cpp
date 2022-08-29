d5e405d26c2684aed85fe88343936b93dde262d7
changqi <84911039+comeonsunny@users.noreply.github.com>
2022-08-29 21:48:01 +0800

    349_intersection_of_two_array

---
+++ Carl_learning/3_hash_table/349_intersection_of_two_arrays.cpp
@@ -0,0 +1,36 @@
+//349 Intersection of Two Arrays
+/*
+    Given two integer arrays nums1 and nums2, return an array of their intersection.
+    Each element in the result must be unique and you may reurn the result in any order.
+*/
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> s2(nums2.begin(), nums2.end());
+        vector<int> res;
+        for (auto i : s1) {
+            if (s2.find(i) != s2.end()) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums1 = {1, 2, 2, 1};
+    vector<int> nums2 = {2, 2};
+    vector<int> res = s.intersection(nums1, nums2);
+    cout << "res: ";
+    for (auto i : res) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
