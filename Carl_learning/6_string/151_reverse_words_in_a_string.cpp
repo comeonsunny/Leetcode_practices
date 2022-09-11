bdf49c254fe757cec9ad2d210419067de46b506a
changqi <84911039+comeonsunny@users.noreply.github.com>
2022-09-11 22:46:32 +0800

    Update 151_reverse_words_in_a_string.cpp
    1. erase all the redundant spaces
    2. reverse whole string
    3. reverse every word in the string

--- Carl_learning/6_string/151_reverse_words_in_a_string.cpp
+++ Carl_learning/6_string/151_reverse_words_in_a_string.cpp
@@ -8,3 +8,41 @@
     The returned string should only have a single space separating the words.
     Do not include any extra spaces.
 */
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        // remove redundant spaces
+        int slow = 0;
+        for (int fast = 0; fast < s.size(); ++fast) {
+            if (s[fast] != ' ') {
+                if (slow != 0) {
+                    s[slow++] = ' ';
+                }
+                while (fast < s.size() && s[fast] != ' ') {
+                    s[slow++] = s[fast++];
+                }
+            }
+        }
+        s.resize(slow);
+        // reverse the whole string
+        reverse(s.begin(), s.end());
+        // reverse each word
+        int start = 0;
+        for (int i = 0; i <= s.size(); ++i) {
+            if (i == s.size() || s[i] == ' ') {
+                reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
+        }
+        return s;
+    }
+};
+int main() {
+    Solution sol;
+    string s = "the sky is blue";
+    cout << sol.reverseWords(s) << endl;
+    return 0;
+}
