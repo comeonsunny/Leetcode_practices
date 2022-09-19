17aad07057a70429419937af430da3e086a7581c
changqi <84911039+comeonsunny@users.noreply.github.com>
2022-09-19 16:54:43 +0800

    Update 232_implement_queue_using_stacks.cpp
    add the implementation of queue by stacks with its back functions.

--- Carl_learning/5_stack/232_implement_queue_using_stacks.cpp
+++ Carl_learning/5_stack/232_implement_queue_using_stacks.cpp
@@ -17,25 +17,57 @@
 #include <stack>
 using namespace std;
 class MyQueue {
+private:
+    stack<int> s_in;
+    stack<int> s_out;
 public:
     MyQueue() {
-
     }
     
     void push(int x) {
-
+      if (s_out.empty()) {
+        s_in.push(x);
+      } else {
+        while (!s_out.empty()) {
+          s_in.push(s_out.top());
+          s_out.pop();
+        }
+        s_in.push(x);
+      }
     }
     
     int pop() {
-
+      int res;
+      if (s_in.empty()) {
+        res = s_out.top();
+        s_out.pop();
+      } else {
+        while (!s_in.empty()) {
+          s_out.push(s_in.top());
+          s_in.pop();
+        }
+        res = s_out.top();
+        s_out.pop();
+      }
+      return res;
     }
     
     int peek() {
-
+      int res;
+      if (s_in.empty()) {
+        res = s_out.top();
+      } else {
+        while (!s_in.empty()) {
+          s_out.push(s_in.top());
+          s_in.pop();
+        }
+        res = s_out.top();
+      }
+      return res;
     }
     
     bool empty() {
-
+      return s_in.empty() && s_out.empty();
     }
 };
 
@@ -47,3 +79,13 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+ int main() {
+   MyQueue* obj = new MyQueue();
+   obj->push(1);
+   obj->push(2);
+   obj->push(3);
+   cout << obj->pop() << endl;
+   cout << obj->peek() << endl;
+   cout << obj->empty() << endl;
+   return 0;
+ }
