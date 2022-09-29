ac27f1ab011689ff09fecdfb5a650245ed768433
changqi <84911039+comeonsunny@users.noreply.github.com>
2022-09-29 22:42:03 +0800

    Create 530_Minimum_absolute_difference_in_BST.cpp
    solution 1: intuitive way

---
+++ Carl_learning/4_binary_tree/530_Minimum_absolute_difference_in_BST.cpp
@@ -0,0 +1,44 @@
+// 530. Minimum Absolute Difference in BST
+/*
+    Given the root of a Binary Search Tree (BST), return the minimum absolute difference 
+    between the values of nay two different nodes in the tree.
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        for (int i = 1; i < this->arr.size(); ++i) {
+            this->res = min(res, arr[i] - arr[i - 1]);
+        }
+        return res;
+    }
+private:
+    vector<int> arr;
+    int res = INT_MAX;
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        arr.push_back(root->val);
+        inorder(root->right);
+    }
+};
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(2);
+    Solution s;
+    cout << s.getMinimumDifference(root) << endl;
+    return 0;
+}
